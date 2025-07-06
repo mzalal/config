ee074f497f7979785ad63581938ba7477e54da00
M. Zalal <mzalal@proton.me>
2025-07-06 23:58:27 +0100

    nvim: Fix code commenting not working in normal mode

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -2,6 +2,10 @@ return {
 	"numToStr/Comment.nvim",
 	config = function()
 		require("Comment").setup({
+			toggler = {
+				line = "<leader>cc",
+				block = "<leader>cb",
+			},
 			opleader = {
 				line = "<leader>cc",
 				block = "<leader>cb",
