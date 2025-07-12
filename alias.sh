da5d611f0ffe1719831b83fee9fc55aa8ee0663d
M. Zalal <mzalal@proton.me>
2025-07-12 14:55:18 +0100

    Add Bash aliases file

---
+++ alias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Projects
+alias on="cd ~/Projects/online_tools/development"
+alias cm="cd ~/Projects/campdown/development"
+
+# Git
+alias gl="git log --oneline"
+alias gll="git long"
+alias gs="git status"
+alias ga="git add"
+alias gaa="git add -A"
+aliad gc="git commit"
+
+# SSH
+alias ssh-primary="ssh-add -D && ssh-add ~/.ssh/id_ed25519"
+alias ssh-secondary="ssh-add -D && ssh-add ~/.ssh/id_ed25519_secondary"
