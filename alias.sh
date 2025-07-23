050b7b745c7bd09c72bad9ad9a327329feef8738
M. Zalal <mzalal@proton.me>
2025-07-24 00:51:11 +0100

    alias: Change projects directories paths

--- alias.sh
+++ alias.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Projects
-alias on="cd ~/Projects/online_tools/development"
-alias cm="cd ~/Projects/campdown/development"
+alias on="cd ~/Projects/online_tools"
+alias cm="cd ~/Projects/campdown"
 
 # Git
 alias gl="git log --oneline"
